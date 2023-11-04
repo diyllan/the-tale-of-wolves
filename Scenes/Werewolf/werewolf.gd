f5018f2a6242bce6fc641dda3a56711172a17f53
Diyllan <dr.manican@gmail.com>
2023-11-04 22:13:13 +0100

    fixed some navigation stuff again

--- Scenes/Werewolf/werewolf.gd
+++ Scenes/Werewolf/werewolf.gd
@@ -146,7 +146,7 @@ func setGravity(delta):
 		
 func update_target_position():
 	if target_vector == last_target_vector:
-		target_vector = Vector3(randi_range(-radius, radius),  0, randi_range(-radius, radius))
+		target_vector = Vector3(randi_range(-radius, radius),  randi_range(-radius, radius), 0)
 		
 func random_roaming():
 	last_target_vector = target_vector 
