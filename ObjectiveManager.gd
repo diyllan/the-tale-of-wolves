a77e87af7f53612551b869f75f584e012ea21208
Diyllan <dr.manican@gmail.com>
2023-11-05 16:16:37 +0100

    fix objectiveManager

--- ObjectiveManager.gd
+++ ObjectiveManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var current_objective: String
-var ObjectiveLabel = get_tree().root.get_node("/root/ViewportShaders/PSXLayer/BlurPostProcess/SubViewport/LCDOverlay/SubViewport/DitherBanding/SubViewport/UI/Objective/ObjectiveTexture/ObjectiveLabel")
+@onready var ObjectiveLabel = get_tree().root.get_node("/root/ViewportShaders/PSXLayer/BlurPostProcess/SubViewport/LCDOverlay/SubViewport/DitherBanding/SubViewport/UI/Objective/ObjectiveTexture/ObjectiveLabel")
 
 func set_Objective(objective:String):
 	ObjectiveLabel.text = objective
