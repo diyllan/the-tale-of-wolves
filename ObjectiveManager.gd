bb9487920bf7ce08c7de7ae3fad55599885adb3b
PainfullySad <141632454+PainfullySad@users.noreply.github.com>
2023-11-06 08:08:29 +0100

    Objective manager created

--- ObjectiveManager.gd
+++ ObjectiveManager.gd
@@ -1,27 +1,29 @@
 extends Node
 
 var current_objective: String
+var general_objective : bool = true
 @onready var ObjectiveLabel = get_tree().root.get_node("/root/ViewportShaders/PSXLayer/BlurPostProcess/SubViewport/LCDOverlay/SubViewport/DitherBanding/SubViewport/UI/Objective/ObjectiveTexture/ObjectiveLabel")
+var world_path : String = "/root/ViewportShaders/PSXLayer/BlurPostProcess/SubViewport/LCDOverlay/SubViewport/DitherBanding/SubViewport/World/"
 
-var objectiveLibrary = {
-	"Talk to the baker": "Baker",
-	"Go fetch the bag of flour": "Flour",
-	"Return to the baker": "Baker",
-	"Go to bed": "Bed",
-	"Talk to the winery owner": "WineDude",
-	"Pick some grapes in the forest": "Grapes",
-	"Return to the winery owner": "WineDude",
-	"Return to bed": "Bed",
-	"Fetch the basket and Latern from the cart": "Basket",
-	"Get the bread from the baker": "Baker",
-	"Get the wine from the winery owner": "WineDude",
-	"Walk to grandma's house at the end of the dark forest": "GrandmaDoor",
-	"Talk to mother": "Mother"
+var objective_library = {
+	"Talk to the baker": "NPC/Villagers/Baker",
+	"Go fetch the bag of flour": "Objectives/Flour",
+	"Return to the baker": "NPC/Villagers/Baker",
+	"Go to bed": "Objectives/Bed",
+	"Talk to the winery owner": "NPC/Villagers/WineDude",
+	"Pick some grapes in the forest": "Objectives/Grapes",
+	"Return to the winery owner": "NPC/Villagers/WineDude",
+	"Return to bed": "Objectives/Bed",
+	"Fetch the basket and Latern from the cart": "Objectives/Basket",
+	"Get the bread from the baker": "NPC/Villagers/Baker",
+	"Get the wine from the winery owner": "NPC/Villagers/WineDude",
+	"Walk to grandma's house at the end of the dark forest": "Objectives/GrandmaDoor",
+	"Talk to mother": "NPC/Villagers/Mother"
 }
+var mother_objective : int = 12
 
 #Dayparts
-var day_part_count = 0;
-var day_count = 1;
+var day_part_count : int = 0;
 
 signal objectiveCompleted
 
@@ -29,4 +31,46 @@ func set_Objective(objective:String):
 	ObjectiveLabel.text = objective
 	
 func load_Next_Objective():
-	pass
+	if (day_part_count == 0 or day_part_count == 4 or day_part_count == 8) and general_objective:
+		#disable objective on the current objective
+		var old_objective = get_tree().root.get_node(world_path + objective_library.values()[mother_objective] + "/StaticBodyInteraction")
+		old_objective.remove_from_group("Objective")
+		#set the new objective text
+		set_Objective(objective_library.keys()[day_part_count])
+		#enable objective on the new objective
+		var new_objective = get_tree().root.get_node(world_path + objective_library.values()[day_part_count] + "/StaticBodyInteraction")
+		new_objective.add_to_group("Objective")
+		#next load = not mother
+		general_objective = false
+		return
+	
+	if (day_part_count == 3 or day_part_count == 7):
+		#next load = mother
+		general_objective = true
+		#disable objective on the current objective
+		var old_objective = get_tree().root.get_node(world_path + objective_library.values()[day_part_count] + "/StaticBodyInteraction")
+		old_objective.remove_from_group("Objective")
+		#increase day part by one
+		day_part_count += 1
+		#set the new objective text
+		set_Objective(objective_library.keys()[mother_objective])
+		#enable objective on the new objective
+		var new_objective = get_tree().root.get_node(world_path + objective_library.values()[mother_objective] + "/StaticBodyInteraction")
+		new_objective.add_to_group("Objective")
+		return
+	
+	if (day_part_count == 11):
+		#no more objectives to complete
+		set_Objective("You finished the game!")
+		return
+	
+	#disable objective on the current objective
+	var old_objective = get_tree().root.get_node(world_path + objective_library.values()[day_part_count] + "/StaticBodyInteraction")
+	old_objective.remove_from_group("Objective")
+	#increase day part by one
+	day_part_count += 1
+	#set the new objective text
+	set_Objective(objective_library.keys()[day_part_count])
+	#enable objective on the new objective -> currently disabled because not all objective models don't exist yet - would error
+	#var new_objective = get_tree().root.get_node(world_path + objective_library.values()[day_part_count] + "/StaticBodyInteraction")
+	#new_objective.add_to_group("Objective")
